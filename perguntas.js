cdc64f14a6f185ff091891391fe81e266b593a31
Diana Aline <00001107705563sp@al.educacao.sp.gov.br>
2024-11-28 17:47:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+
+criaCartao(
+    'maquiagem',
+    'qual produto é usado para hidratar a pele ?',
+    'hidratante facial'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'matemática',
+    'Quabtos q é 20x8?',
+    '160s'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
